03c23e4a11b14de0ffecc05b86b2ecf0d8b83461
Licshee <licshee@yen.ai>
2016-03-31 16:17:39 +0800

    .

--- TextArgs.cmd
+++ TextArgs.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd Release
-powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\%~n0.nuspec" Portable\%~n0
+powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\%~n0.nuspec" "Portable\%~n0"
 popd
 echo.
 echo Done.
